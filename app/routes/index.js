5b39a38fd3d4bf508e801e75b3a35dc3c702561b
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-10-19 19:40:35 -0200

    app listen

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,5 @@
 const noteRoutes = require('./blog_routes');
-
-module.exports = function(app, db){
+module.exports = function (app, db) {
     noteRoutes(app, db);
-    
     //other routes group should go here
 };
-

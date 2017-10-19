d97964cd2df7674cb1e9604a7d1568bd324544f9
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-10-19 20:32:17 -0200

    fix url heroku

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
+
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-//app.listen(8000);
+
 
 app.listen(process.env.PORT || 8080, () => {
             console.log('We are live on ' + port);
@@ -16,7 +19,9 @@ app.listen(process.env.PORT || 8080, () => {
 
 console.log(" subiu no heroku ");
 
-MongoClient.connect("mongodb://heroku_f7vnmk6r:heroku_f7vnmk6r@ ds125555.mlab.com:25555/heroku_f7vnmk6r", (err, database) => {
+
+
+MongoClient.connect("mongodb://heroku_f7vnmk6r:dipmsrg72u14je61vc3ebpg4r@ds125555.mlab.com:25555/heroku_f7vnmk6r", (err, database) => {
         if (err) return console.log(err)
         require('./app/routes')(app, database);
         

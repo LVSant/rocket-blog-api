3a7c6b4177de8b82c3cc2b6b7d31222cc42efc9a
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-10-19 19:25:32 -0200

    changing port

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 8000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*MongoClient.connect("mongodb://blogctsdb:blogctsdb@ds125255.mlab.com:25255/blogctsdb", (err, database) => {
+//app.listen(8000);
+
+MongoClient.connect("mongodb://blogctsdb:blogctsdb@ds125255.mlab.com:25255/blogctsdb", (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });               
-})*/
+})
 
-console.log("Hello World");
+//console.log("Hello World");

890a449ddce535edf7e1d0265965068c949764b9
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-10-18 23:32:52 -0200

    added heroku

--- app/routes/blog_routes.js
+++ app/routes/blog_routes.js
@@ -1,7 +1,19 @@
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body)
-      
-    res.send('Hello')
-  });
+module.exports = function (app, db) {
+    app.post('/addblog', (req, res) => {
+        const note = {
+            text: req.body.body
+            , title: req.body.title
+        };
+        
+        db.collection('blog').insert(note, (err, result) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            }
+            else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
 };

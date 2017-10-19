eb2133d4d4956adab9e5c9a6494b3f65555fe289
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-10-19 20:50:38 -0200

    try heroku

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
 const app = express();
 const port = 8080;
 
+
+var uri = 'mongodb://heroku_f7vnmk6r:dipmsrg72u14je61vc3ebpg4r@ds125555.mlab.com:25555/heroku_f7vnmk6r';
 
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
 
 
 app.listen(process.env.PORT || 8080, () => {
@@ -21,7 +25,7 @@ console.log(" subiu no heroku ");
 
 
 
-MongoClient.connect("mongodb://heroku_f7vnmk6r:dipmsrg72u14je61vc3ebpg4r@ds125555.mlab.com:25555/heroku_f7vnmk6r", (err, database) => {
+mongodb.MongoClient.connect(uri, (err, database) => {
         if (err) return console.log(err)
         require('./app/routes')(app, database);
         

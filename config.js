4ee647014e34fc65c1dd8432a6b49034c40c4bc0
leonardo viveiros <leonardoviveirossantos@gmail.com>
2017-11-02 17:00:57 -0200

    fixing files

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'jwtSecret': process.env.JWTSECRET ,
-    'jwtSession': {session: false},
-    'database': process.env.MONGODB_URI ,
-    'userAdminPassword' : process.env.SUPERSECRET_PASSWD 
-};

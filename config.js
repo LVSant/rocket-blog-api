d2b7aeaaade31793b3da4d3c5a7249c0e6f95e0d
leonardo viveiros <leonardoviveirossantos@gmail.com>
2017-11-02 16:51:51 -0200

    fix configg

--- config.js
+++ config.js
@@ -2,9 +2,5 @@ module.exports = {
     'jwtSecret': process.env.JWTSECRET,
     'jwtSession': {session: false},
     'database': process.env.MONGODB_URI,
-    'userAdminPassword' : process.env.SUPERSECRET_PASSWD
-<<<<<<< HEAD
+    'userAdminPassword': process.env.SUPERSECRET_PASSWD
 };
-=======
-};
->>>>>>> 12b8c3de973a64f340fc754ae382d85301ae8b7c

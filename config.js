e88f1a6a35b11373140501e0f8b2ac45c2748b97
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-11-02 17:12:58 -0200

    wow

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    'jwtSecret': process.env.JWTSECRET || '0727FCALjkVA48PCxPPtyaPBsWid1MU6PzOHLNUsXRV8ibR7NVo6pTCwnxFivP6IKGF78G',
+    'jwtSecret': process.env.JWTSECRET,
     'jwtSession': {session: false},
-    'database': process.env.MONGODB_URI || 'mongodb://mongo_admin:mongo_admin@localhost:27017/rocketBlogAPITest',
-    'userAdminPassword' : process.env.SUPERSECRET_PASSWD || 'ab'
+    'database': process.env.MONGODB_URI,
+    'userAdminPassword' : process.env.SUPERSECRET_PASSWD
 };

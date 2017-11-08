d0589e4ff6ad2de7a1f32d3211a4bc10c3b8edc1
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2017-11-08 13:32:31 -0200

    adding test mongodb

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     'jwtSecret': process.env.JWTSECRET,
     'jwtSession': {session: false},
     'database': process.env.MONGODB_URI,
+    'databaseTest': process.env.MONGODB_TEST_URI ,
     'userAdminPassword' : process.env.SUPERSECRET_PASSWD
 };
